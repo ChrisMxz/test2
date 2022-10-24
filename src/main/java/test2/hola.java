316093a2ee379e5a8d9d769e3d661735e80a88c2
David <cristhegood15@gmail.com>
2022-10-24 10:22:42 -0500

    3er commit desde rama dev

--- src/main/java/test2/hola.java
+++ src/main/java/test2/hola.java
@@ -4,11 +4,14 @@ public class hola {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//desde la rama dev
+		// desde la rama dev
 		System.out.print("DEV prueba de commit");
-		
-		//nuevo commit desde ramma dev
+
+		// nuevo commit desde ramma dev
 		System.out.print("dev commit 2");
+
+		// nuevo commit desde ramma dev
+		System.out.print("dev commit 3");
 
 	}
 
