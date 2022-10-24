1134838c0634ef7a5125888cb0c436deb9bca3d9
David <cristhegood15@gmail.com>
2022-10-24 10:14:28 -0500

    Nuevo commit desde rama dev

--- src/main/java/test2/hola.java
+++ src/main/java/test2/hola.java
@@ -4,10 +4,8 @@ public class hola {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("prueba tortoise");
-		
-		//nuevo commit
-		System.out.print("Segundo commit para probar ");
+		//desde la rama dev
+		System.out.print("DEV prueba de commit");
 
 	}
 
