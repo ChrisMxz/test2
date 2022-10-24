ed31b6c7468012c6707b347b69b64a21996d767e
David <cristhegood15@gmail.com>
2022-10-24 10:15:51 -0500

    commit 2

--- src/main/java/test2/hola.java
+++ src/main/java/test2/hola.java
@@ -6,6 +6,9 @@ public class hola {
 		// TODO Auto-generated method stub
 		//desde la rama dev
 		System.out.print("DEV prueba de commit");
+		
+		//nuevo commit desde ramma dev
+		System.out.print("dev commit 2");
 
 	}
 
